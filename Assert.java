fad22ad6b8dd7ee94d6e0a09d24ab663ac6ec0b6
ShubhamKeshri334 <shubhamkeshri334@gmail.com>
2025-04-11 19:04:40 +0530

    assert

---
+++ Assert.java
@@ -0,0 +1,20 @@
+package assertKeyword;
+
+public class Assert {//Assert keyword is use to terminate the programm
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int a=1;a<=10;a++) {
+			assert a<=5:"terminate";//if the condition is true it will let the program to run and if the condition becomes false it will terminate the program.
+			System.out.println("Run the program:");
+			
+			
+		}
+		/*int age=10;
+		assert age>=18;//"sorry";
+		System.out.println("you can vote");
+	}*/
+
+}
+}
